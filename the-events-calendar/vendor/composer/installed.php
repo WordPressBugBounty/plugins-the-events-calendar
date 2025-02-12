838265fdfe905b04e289c205752ef207b54638d0
Sean Murphy <sean@iamseanmurphy.com>
2025-02-12 19:16:50 +0000

    6.10.1.1

--- the-events-calendar/vendor/composer/installed.php
+++ the-events-calendar/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/the-events-calendar',
-        'pretty_version' => 'dev-release/T25.antman',
-        'version' => 'dev-release/T25.antman',
-        'reference' => 'cbf9dca7a9452dfddbf8009632937679740583df',
+        'pretty_version' => 'dev-release/T25.antman.1',
+        'version' => 'dev-release/T25.antman.1',
+        'reference' => '7d9941866eac59d3302c275ee99ec113cb9b68ea',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/the-events-calendar' => array(
-            'pretty_version' => 'dev-release/T25.antman',
-            'version' => 'dev-release/T25.antman',
-            'reference' => 'cbf9dca7a9452dfddbf8009632937679740583df',
+            'pretty_version' => 'dev-release/T25.antman.1',
+            'version' => 'dev-release/T25.antman.1',
+            'reference' => '7d9941866eac59d3302c275ee99ec113cb9b68ea',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
