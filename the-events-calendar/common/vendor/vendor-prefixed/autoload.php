d98bfc4fd6e04d15cf74a531c6bef8405a76ac7c
Sean Murphy <sean@iamseanmurphy.com>
2026-06-24 22:40:15 +0000

    6.16.5

--- the-events-calendar/common/vendor/vendor-prefixed/autoload.php
+++ the-events-calendar/common/vendor/vendor-prefixed/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3f9f88c17b56a178537d3207f6e012e5::getLoader();
+return ComposerAutoloaderInit9df69553aa819261eea7e6a66af2a831::getLoader();
